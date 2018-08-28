bffd15cbe6b97d0dceb5199487b1dc4e8dc7c28b
ISISPACE <a.miraglia@isispace.nl>
2018-08-28 12:15:25 +0200

    V40.1 qa_pll with containers
    improved PLL test with containers

--- python/qa_pll.py
+++ python/qa_pll.py
@@ -7,15 +7,17 @@
 from gnuradio import gr, gr_unittest
 from gnuradio import blocks, analog
 from gnuradio import fft
+from collections import namedtuple
 from gnuradio.fft import window
 import ecss_swig as ecss
+import flaress
 import math, time, datetime, os, abc, sys
 import runner
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
-def get_parameters(data):
+def print_parameters(data):
     to_print = "\p Order= %d, Coeff1 (2nd order)= %f, Coeff2 (2nd order)= %f, Coeff4 (2nd order)= %f, Coeff1 (3rd order)= %f, Coeff2 (3rd order)= %f, Coeff3 (3rd order)= %f, Sample rate = %d, Input frequency = %d, Input noise = %f \p" \
         %(data.order, data.coeff1_2, data.coeff2_2, data.coeff2_4, data.coeff1_3, data.coeff2_3, data.coeff3_3, data.samp_rate, data.freq, data.noise)
     return to_print
@@ -24,22 +26,23 @@ def test_sine(self, data):
     """this function run the defined test, for easier understanding"""
 
     tb = self.tb
+    data_pll = namedtuple('data_pll', 'src out freq pe pa')
 
     amplitude = 1
     offset = 0
 
-    src_sine = analog.sig_source_c(data.samp_rate, analog.GR_CONST_WAVE, data.freq, amplitude, offset)
+    src_sine = analog.sig_source_c(data.samp_rate, analog.GR_SIN_WAVE, data.freq, amplitude, offset)
     src_noise = analog.noise_source_c(analog.GR_GAUSSIAN, data.noise, offset)
 
     adder = blocks.add_vcc(1)
     throttle = blocks.throttle(gr.sizeof_gr_complex*1, data.samp_rate,True)
-    head = blocks.head(gr.sizeof_gr_complex, int (data.N))
+    head = blocks.head(gr.sizeof_gr_complex, int (data.items))
 
     dst_source = blocks.vector_sink_c()
     dst_pll_out = blocks.vector_sink_c()
     dst_pll_freq = blocks.vector_sink_f()
     dst_pll_pe = blocks.vector_sink_f()
-    dst_pll_pa = blocks.vector_sink_int64()
+    dst_pll_pa = flaress.vector_sink_int64()
 
     pll = ecss.pll(data.samp_rate, data.order, data.N, data.coeff1_2, data.coeff2_2, data.coeff2_4, data.coeff1_3, data.coeff2_3, data.coeff3_3, data.f_central, data.bw)
 
@@ -54,17 +57,20 @@ def test_sine(self, data):
     tb.connect((pll, 2), dst_pll_pe)
     tb.connect((pll, 3), dst_pll_pa)
 
+    # throttle.set_max_noutput_items (data.samp_rate)
+    # throttle.set_min_noutput_items (data.samp_rate)
+
     self.tb.run()
 
-    data_pll = dst_pll_out.data()
-
-    # data_pll.in = dst_source.data()
-    # data_pll.out = dst_pll_out.data()
-    # data_pll.freq = dst_pll_freq.data()
-    # data_pll.pe = dst_pll_pe.data()
-    # data_pll.pa = dst_pll_pa.data()
+    data_pll.src = dst_source.data()
+    data_pll.out = dst_pll_out.data()
+    data_pll.freq = dst_pll_freq.data()
+    data_pll.pe = dst_pll_pe.data()
+    data_pll.pa = dst_pll_pa.data()
 
     return data_pll
+
+
 
 
 class qa_pll (gr_unittest.TestCase):
@@ -77,30 +83,35 @@ class qa_pll (gr_unittest.TestCase):
 
     def test_001_t (self):
         """Test 1 """
+        param = namedtuple('param', 'order coeff1_2 coeff2_2 coeff2_4 coeff1_3 coeff2_3 coeff3_3 f_central bw samp_rate items N freq noise')
 
-        data.order =  2
-        data.coeff1_2 = 0.1
-        data.coeff2_2 = 0.001
-        data.coeff2_4 = 1
-        data.coeff1_3 = 0.01
-        data.coeff2_3 = 0.00001
-        data.coeff3_3 = 0.0000001
-        data.f_central = 1000
-        data.bw = 2000
-        data.samp_rate = 32000
-        data.N = 4000
-        data.freq = 1000
-        data.noise = 0
+        param.order = 2
+        param.coeff1_2 = 0.1
+        param.coeff2_2= 0.001
+        param.coeff2_4= 1
+        param.coeff1_3= 0.01
+        param.coeff2_3= 0.00001
+        param.coeff3_3= 0.00000001
+        param.f_central = 500
+        param.bw = 500
+        param.N = 38
+        param.samp_rate = 4192
+        param.items = 4192 * 2
+        param.freq = 500
+        param.noise = 0
+
+
 
         # print get_parameters(data)
-
         print "\p weeeeeeee \p"
 
-        data_out = test_sine(self, data)
+
+        data = test_sine(self, param)
 
         self.assertEqual(0, 0)
 
-        print "hey"
+        print data.pe
+
 
 
 if __name__ == '__main__':
