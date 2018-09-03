97da9a5ed5478537762d9787af264d1066a01cdf
ISISPACE <a.miraglia@isispace.nl>
2018-09-03 10:05:41 +0200

    V44.1 added switch test
    added test for the switch from 2 to 3 order with the debug func probe 
    block

--- python/qa_pll.py
+++ python/qa_pll.py
@@ -92,7 +92,7 @@ def plot_fft(self, data_fft):
     # plt.show()
     self.pdf.add_to_pdf(fig)
 
-def check_comlex(data_out, real_final, imag_final, error):
+def check_complex(data_out, real_final, imag_final, error):
     """this function checks the complex output data from the pll. It checks the time to reach the final value with the expected error (if the output reachs it), and the error/accuracy of the outputs"""
 
     if (abs(data_out[-1].real - real_final) > abs(error)) or (abs(data_out[-1].imag - imag_final) > abs(error)): #check if is reached the final value at the end
@@ -100,6 +100,7 @@ def check_comlex(data_out, real_final, imag_final, error):
 
     imag_error_max = 0
     real_error_max = 0
+    settling_time_index = np.inf
 
     for i in reversed(xrange (len(data_out))):
         if (abs(data_out[i].real - real_final) > abs(real_error_max)):
@@ -375,7 +376,7 @@ class qa_pll (gr_unittest.TestCase):
     #     plot_fft(self,data_fft)
     #
     #     #check output 'out'
-    #     out_settling_time_index, out_real_error_max, out_imag_error_max = check_comlex(data_sine.out, 1, 0, 0.1)
+    #     out_settling_time_index, out_real_error_max, out_imag_error_max = check_complex(data_sine.out, 1, 0, 0.1)
     #     out_settling_time_ms = (1.0 / param.samp_rate) * out_settling_time_index * 1000.0
     #     self.assertLess(out_settling_time_ms, np.inf) #errors are intrinsically asserted
     #     print "-Output 'Out' Settling time : %f ms;" % out_settling_time_ms
@@ -436,7 +437,7 @@ class qa_pll (gr_unittest.TestCase):
     #     plot_fft(self,data_fft)
     #
     #     #check output 'out'
-    #     out_settling_time_index, out_real_error_max, out_imag_error_max = check_comlex(data_sine.out, 1, 0, 0.1)
+    #     out_settling_time_index, out_real_error_max, out_imag_error_max = check_complex(data_sine.out, 1, 0, 0.1)
     #     out_settling_time_ms = (1.0 / param.samp_rate) * out_settling_time_index * 1000.0
     #     self.assertLess(out_settling_time_ms, np.inf) #errors are intrinsically asserted
     #     print "-Output 'Out' Settling time : %f ms;" % out_settling_time_ms
@@ -497,9 +498,9 @@ class qa_pll (gr_unittest.TestCase):
     #     plot_fft(self,data_fft)
     #
     #     #check output 'out'
-    #     out_settling_time_index, out_real_error_max, out_imag_error_max = check_comlex(data_sine.out, 1, 0, 0.1)
+    #     out_settling_time_index, out_real_error_max, out_imag_error_max = check_complex(data_sine.out, 1, 0, 0.1)
     #     out_settling_time_ms = (1.0 / param.samp_rate) * out_settling_time_index * 1000.0
-    #     self.assertEqual(settling_time_ms, np.inf) #have to be be inf (so, unlocked), errors are intrinsically asserted
+    #     self.assertEqual(out_settling_time_ms, np.inf) #have to be be inf (so, unlocked), errors are intrinsically asserted
     #     print "-Output 'Out' Settling time : %f ms;" % out_settling_time_ms
     #     print "-Output 'Out' Real absolute maximum error: %.3f;" % out_real_error_max
     #     print "-Output 'Out' Imag absolute maximum error: %.3f." % out_imag_error_max
@@ -616,96 +617,112 @@ class qa_pll (gr_unittest.TestCase):
         param.N = 38
         param.fft_size = 1024
         param.samp_rate = 4096
-        param.items = 4096 * 4
-        param.freq = 1000
+        param.items = 4096 * 6
+        param.freq = 600
         param.noise = 0
 
-        # print_parameters(param)
+        print_parameters(param)
 
         amplitude = 1
         offset = 0
 
-        # pll = ecss.pll(param.samp_rate, param.order, param.N, param.coeff1_2, param.coeff2_2, param.coeff2_4, param.coeff1_3, param.coeff2_3, param.coeff3_3, param.f_central, param.bw)
-        src = analog.sig_source_f(param.samp_rate, analog.GR_CONST_WAVE, param.freq, 0, 0)
-        debug = flaress.debug_func_probe(gr.sizeof_float*1)
+        pll = ecss.pll(param.samp_rate, param.order, param.N, param.coeff1_2, param.coeff2_2, param.coeff2_4, param.coeff1_3, param.coeff2_3, param.coeff3_3, param.f_central, param.bw)
+        debug_switch = flaress.debug_func_probe(gr.sizeof_gr_complex*1)
 
         def _probe_func_probe():
-            time.sleep(2)
+            time.sleep(3) #in the middle of one block of items, to be more sure that both functions are executed in the at the same time.
             try:
-            #     # pll.set_order(3)
-            #     # src.set_offset(1)
-                debug.debug_nitems()
+                pll.set_order(3)
+                debug_switch.debug_nitems()
+                self.debug_order = pll.get_order()
             except AttributeError:
                 pass
-
         _probe_func_thread = threading.Thread(target=_probe_func_probe)
         _probe_func_thread.daemon = True
 
-        throttle = blocks.throttle(gr.sizeof_float*1, param.samp_rate,True)
-        head = blocks.head(gr.sizeof_float, int (param.items))
-        dst_source = blocks.vector_sink_f()
-        src_null = blocks.null_source(gr.sizeof_float*1)
+        src_sine = analog.sig_source_c(param.samp_rate, analog.GR_SIN_WAVE, param.freq, amplitude, offset)
+        src_noise = analog.noise_source_c(analog.GR_GAUSSIAN, param.noise, offset)
 
+        adder = blocks.add_vcc(1)
+        throttle = blocks.throttle(gr.sizeof_gr_complex*1, param.samp_rate,True)
+        head = blocks.head(gr.sizeof_gr_complex, param.items)
+
+        dst_source = blocks.vector_sink_c()
+        dst_pll_out = blocks.vector_sink_c()
+        dst_pll_freq = blocks.vector_sink_f()
+        dst_pll_pe = blocks.vector_sink_f()
+        dst_pll_pa = flaress.vector_sink_int64()
 
         throttle.set_max_noutput_items (param.samp_rate)
         throttle.set_min_noutput_items (param.samp_rate)
 
-        tb.connect(src, throttle)
+        tb.connect(src_sine, (adder, 0))
+        tb.connect(src_noise,(adder, 1))
+        tb.connect(adder, throttle)
         tb.connect(throttle, head)
         tb.connect(head, dst_source)
-        tb.connect(head, debug)
+        tb.connect(head, debug_switch)
+        tb.connect(head, pll)
+        tb.connect((pll, 0), dst_pll_out)
+        tb.connect((pll, 1), dst_pll_freq)
+        tb.connect((pll, 2), dst_pll_pe)
+        tb.connect((pll, 3), dst_pll_pa)
 
         _probe_func_thread.start()
+        self.tb.run()
 
-        tb.run()
+        data_pll.src = dst_source.data()
+        data_pll.out = dst_pll_out.data()
+        data_pll.freq = dst_pll_freq.data()
+        data_pll.pe = dst_pll_pe.data()
+        data_pll.pa = dst_pll_pa.data()
+        data_pll.time = np.linspace(0, (param.items * 1.0 / param.samp_rate), param.items, endpoint=False)
 
-        data = debug.data()
+        switch = debug_switch.data()
 
-        print data
+        plot(self,data_pll)
 
-        # src_sine = analog.sig_source_c(param.samp_rate, analog.GR_SIN_WAVE, param.freq, amplitude, offset)
-        # src_tag = blocks.tags_strobe(gr.sizeof_gr_complex*1, pmt.intern("reset"), (param.items / 2),  pmt.intern("pll"))
-        #
-        # adder = blocks.add_vcc(1)
-        # throttle = blocks.throttle(gr.sizeof_gr_complex*1, param.samp_rate,True)
-        # head = blocks.head(gr.sizeof_gr_complex, int (param.items))
-        #
-        # dst_source = blocks.vector_sink_c()
-        # dst_pll_out = blocks.vector_sink_c()
-        # dst_pll_freq = blocks.vector_sink_f()
-        # dst_pll_pe = blocks.vector_sink_f()
-        # dst_pll_pa = flaress.vector_sink_int64()
-        #
-        # throttle.set_max_noutput_items (param.samp_rate)
-        # throttle.set_min_noutput_items (param.samp_rate)
-        #
-        # tb.connect(src_sine, (adder, 0))
-        # tb.connect(src_tag,(adder, 1))
-        # tb.connect(adder, throttle)
-        # tb.connect(throttle, head)
-        # tb.connect(head, dst_source)
-        # tb.connect(head, pll)
-        # tb.connect((pll, 0), dst_pll_out)
-        # tb.connect((pll, 1), dst_pll_freq)
-        # tb.connect((pll, 2), dst_pll_pe)
-        # tb.connect((pll, 3), dst_pll_pa)
-        #
-        # self.tb.run()
-        #
-        # data_pll.src = dst_source.data()
-        # data_pll.out = dst_pll_out.data()
-        # data_pll.freq = dst_pll_freq.data()
-        # data_pll.pe = dst_pll_pe.data()
-        # data_pll.pa = dst_pll_pa.data()
-        # data_pll.time = np.linspace(0, (param.items * 1.0 / param.samp_rate), param.items, endpoint=False)
-        #
-        # plot(self,data_pll)
-        #
-        # #the reset tag initializes the pll at the central frequency (so all the internal registers of the loop filter) and sets to zero the phase accumulator
-        # self.assertAlmostEqual(data_pll.freq[param.items / 2], param.f_central, 0) #only the integer part
-        # self.assertAlmostEqual(data_pll.pa[param.items / 2], 0)
-        #
-        # print "-Reset tag received at the moment: %.3f ms." % (param.items / 2 * (1000.0 / param.samp_rate))
+        param.items = 4096 * 8
+
+        data_fft = test_fft(self, param)
+        plot_fft(self,data_fft)
+
+        #check the switch
+        self.assertEqual(len(switch), 1)
+        self.assertEqual(self.debug_order, 3)
+        print "-Final order of the pll: %d" %self.debug_order
+        print "-Set function received at the moment (of the simulation): %.2f s." % (switch[0] * (1.0 / param.samp_rate))
+
+        #check output 'out'
+        out_settling_time_index, out_real_error_max, out_imag_error_max = check_complex(data_pll.out, 1, 0, 0.1)
+        out_settling_time_ms = (1.0 / param.samp_rate) * out_settling_time_index * 1000.0
+        self.assertLess(out_settling_time_ms, np.inf) #errors are intrinsically asserted
+        print "-Output 'Out' Settling time : %f ms;" % out_settling_time_ms
+        print "-Output 'Out' Real absolute maximum error: %.3f;" % out_real_error_max
+        print "-Output 'Out' Imag absolute maximum error: %.3f." % out_imag_error_max
+
+        #check output 'out'
+        pe_settling_time_index, pe_error_max = check_float(data_pll.pe, 0, 0.1)
+        pe_settling_time_ms = (1.0 / param.samp_rate) * pe_settling_time_index * 1000.0
+        self.assertLess(pe_settling_time_ms, np.inf) #errors are intrinsically asserted
+        print "-Output 'pe' Settling time : %f ms;" % pe_settling_time_ms
+        print "-Output 'pe' absolute maximum error: %.3f;" % pe_error_max
+
+        #check output 'out'
+        freq_settling_time_index, freq_error_max = check_float(data_pll.freq, param.freq, (param.freq * 0.05)) #check if the measured output frequency is the same of the input signal ± 5%
+        freq_settling_time_ms = (1.0 / param.samp_rate) * freq_settling_time_index * 1000.0
+        self.assertLess(freq_settling_time_ms, np.inf) #errors are intrinsically asserted
+        print "-Output 'freq' Settling time : %f ms;" % freq_settling_time_ms
+        print "-Output 'freq' absolute maximum error: %.3f;" % freq_error_max
+
+        #check output 'pa'
+        pa_min_step , pa_slope = check_pa(data_pll.pa, 100)
+        precision = math.pow(2,(- (param.N - 1))) * math.pi
+        pa_min_step_rad = (pa_min_step >> (64 - param.N)) * precision
+        pa_slope_rad = (pa_slope >> (64 - param.N)) * precision
+        self.assertGreaterEqual(pa_min_step_rad, precision)
+        print "-Output 'pa' Slope : %f rad/s;" % pa_slope_rad       # WARNING: this is only a mean
+        print "-Output 'pa' Min step : %f rad;" % pa_min_step_rad
 
 if __name__ == '__main__':
     suite = gr_unittest.TestLoader().loadTestsFromTestCase(qa_pll)
