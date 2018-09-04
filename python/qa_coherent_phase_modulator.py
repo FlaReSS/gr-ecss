f3616027862149c1f289645b8e79c9f78a13c780
ISISPACE <a.miraglia@isispace.nl>
2018-09-04 09:32:59 +0200

    V45.1  qa coh phase mod

--- python/qa_coherent_phase_modulator.py
+++ python/qa_coherent_phase_modulator.py
@@ -4,9 +4,54 @@
 #
 
 from gnuradio import gr, gr_unittest
-from gnuradio import blocks
-import runner
+from gnuradio import blocks, analog
+from gnuradio.fft import logpwrfft
+from collections import namedtuple
+from gnuradio.fft import window
 import ecss_swig as ecss
+import flaress
+import math, time, datetime, os, abc, sys, pmt
+import runner, threading
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+class Pdf_class(object):
+    """this class can print a single pdf for all the tests"""
+
+    graphs_list = []
+
+    def __init__(self, name_test='test'):
+        current_dir = os.getcwd()
+        dir_to = os.path.join(current_dir, 'Graphs')
+
+        if not os.path.exists(dir_to):
+            os.makedirs(dir_to)
+        self.name_test = name_test.split('.')[0]
+        self.name_complete = dir_to + '/' + name_test.split('.')[0] + "_graphs.pdf"
+
+    def add_to_pdf(self, fig):
+        """this function can add a new element/page to the list of pages"""
+
+        fig_size = [21 / 2.54, 29.7 / 2.54] # width in inches & height in inches
+        fig.set_size_inches(fig_size)
+        Pdf_class.graphs_list.append(fig)
+
+    def finalize_pdf(self):
+        """this function print the final version of the pdf with all the pages"""
+
+        with PdfPages(self.name_complete) as pdf:
+            for graph in Pdf_class.graphs_list:
+                pdf.savefig(graph)   #write the figures for that list
+
+            d = pdf.infodict()
+            d['Title'] = self.name_test
+            d['Author'] = 'Antonio Miraglia - ISISpace'
+            d['Subject'] = 'self generated graphs from the qa test'
+            d['Keywords'] = self.name_test
+            d['CreationDate'] = datetime.datetime(2018, 8, 21)
+            d['ModDate'] = datetime.datetime.today()
+
 
 class qa_coherent_phase_modulator (gr_unittest.TestCase):
 
