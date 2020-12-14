15a6375df269eec0cceadc590e5028a5acbe3e26
Alberto Busso <iw1fnw@gmail.com>
2020-12-14 08:40:56 +0100

    cleaned up install script

--- install_module.sh
+++ install_module.sh
@@ -2,21 +2,31 @@
 # Script to install a module in gnuradio.
 #run it in the main folder of the OOT module
 
-if [ $1 = '-make' ]; then
+if [ "$1" = "-make" ]; then
 	source ~/prefix/setup_env.sh
 	rm -r build
 	mkdir build
 	cd build
 	cmake -DCMAKE_INSTALL_PREFIX=~/prefix ../
 	make
-elif [ $1 = '-test' ]; then
+elif [ "$1" = "-test" ]; then
+	source ~/prefix/setup_env.sh
+	rm -r build
+	mkdir build
+	cd build
+	cmake -DCMAKE_INSTALL_PREFIX=~/prefix ../
 	make
 	make test
-elif [ $1 = '-install' ]; then
+elif [ "$1" = "-notest" ]; then
+	source ~/prefix/setup_env.sh
+	rm -r build
+	mkdir build
+	cd build
+	cmake -DCMAKE_INSTALL_PREFIX=~/prefix ../
 	make
 	sudo make install
 	sudo ldconfig
-elif [ $1 = '-complete' ]; then
+elif [ "$1" = "-all" ]; then
 	source ~/prefix/setup_env.sh
 	rm -r build
 	mkdir build
@@ -27,5 +37,5 @@ elif [ $1 = '-complete' ]; then
 	sudo make install
 	sudo ldconfig
 else
-	echo Please, specify between -make, -test, -install, -complete
+	echo Please, specify between -make, -test, -notest, -all
 fi
